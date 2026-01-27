4a951828d4d9c1776938ca4390a5d8998b38bebc
prushti <prushtipatel0407@gmail.com>
2026-01-27 14:27:05 +0530

    Some updates

--- auth_project/business/admin.py
+++ auth_project/business/admin.py
@@ -1,3 +1,13 @@
+# business/admin.py
 from django.contrib import admin
+from .models import BusinessCategory, BusinessProfile
 
-# Register your models here.
+@admin.register(BusinessCategory)
+class BusinessCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(BusinessProfile)
+class BusinessProfileAdmin(admin.ModelAdmin):
+    list_display = ("business_name", "category", "city", "country")
--- auth_project/business/models.py
+++ auth_project/business/models.py
@@ -3,6 +3,14 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+
+class BusinessCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class BusinessProfile(models.Model):
     user = models.OneToOneField(
         User,
@@ -11,21 +19,21 @@ class BusinessProfile(models.Model):
     )
 
     business_name = models.CharField(max_length=255)
-    category = models.CharField(max_length=100)
+
+    category = models.ForeignKey(
+        BusinessCategory,
+        on_delete=models.PROTECT,
+        related_name="businesses"
+    )
 
     website = models.URLField(blank=True, null=True)
 
-    # Address (structured)
     building_name = models.CharField(max_length=255, blank=True)
     street = models.CharField(max_length=255)
-    area = models.CharField(max_length=255, blank=True)
 
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20, blank=True)
-
-    timezone = models.CharField(max_length=50, default="UTC")
 
     created_at = models.DateTimeField(auto_now_add=True)
 
